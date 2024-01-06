8a5ac860bd5f5ee7ae2409a304c6f017852742a9
ross-brown <rbrown_98@yahoo.com>
2024-01-05 22:00:13 -0600

    add light/dark mode

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Combines multiple class values using the clsx library and merges them
+ * with Tailwind CSS utility classes using twMerge.
+ *
+ * @param inputs - The class values to be combined.
+ * @returns A string representing the merged and formatted class names.
+ *
+ * @example
+ * // Returns "text-red-500 bg-blue-700 my-2"
+ * cn("text-red-500", "bg-blue-700", "my-2");
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
