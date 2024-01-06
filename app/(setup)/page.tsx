8a1eeee55de46e9ae327b939c21cade40fd5c2cd
ross-brown <rbrown_98@yahoo.com>
2024-01-05 23:35:02 -0600

    create initial modal for creating server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import InitialModal from "@/components/modals/initial-modal";
 
 /** SetupPage Component
  *
@@ -24,11 +25,7 @@ async function SetupPage() {
 
   if (server) return redirect(`/servers/${server.id}`);
 
-  return (
-    <div>
-      Create a server
-    </div>
-  );
+  return <InitialModal />
 }
 
 export default SetupPage;
