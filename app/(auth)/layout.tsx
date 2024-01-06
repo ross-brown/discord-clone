4ba6ae2b98320003733b18b9dcdd89a56fab1134
ross-brown <rbrown_98@yahoo.com>
2024-01-05 20:10:11 -0600

    setup basic folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+function AuthLayout({ children }: { children: React.ReactNode; }) {
+  return (
+    <div className="bg-red-500 h-full">
+      {children}
+    </div>
+  );
+}
+
+export default AuthLayout;
