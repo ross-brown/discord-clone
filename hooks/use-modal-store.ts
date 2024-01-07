71292cb7cdd4b0a012f9ea3248e91e2a694c66cf
ross-brown <rbrown_98@yahoo.com>
2024-01-07 15:46:56 -0600

    create server modal

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+export type ModalType = "createServer";
+
+interface ModalStore {
+  type: ModalType | null;
+  isOpen: boolean;
+  onOpen: (type: ModalType) => void;
+  onClose: () => void;
+}
+
+/** Hook for managing modal state
+ *
+ * Return object with props/methods { type, isOpen, onOpen, onClose }
+*/
+export const useModal = create<ModalStore>((set) => ({
+  type: null,
+  isOpen: false,
+  onOpen: (type) => set({ isOpen: true, type }),
+  onClose: () => set({ type: null, isOpen: false })
+}));
