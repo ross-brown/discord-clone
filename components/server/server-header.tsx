2a3e7e9c05c46d91cc3b173e058138f7009418d2
ross-brown <rbrown_98@yahoo.com>
2024-01-08 16:18:09 -0600

    implement create channel modal and api route

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -63,6 +63,7 @@ function ServerHeader({ server, role }: ServerHeaderProps) {
         )}
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("createChannel")}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Create Channel
