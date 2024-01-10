1c1e8e7d3c61d66e77ccf1634e390d1c50ac9805
ross-brown <rbrown_98@yahoo.com>
2024-01-09 22:10:05 -0600

    add emoji picker to chat

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -11,9 +11,11 @@ import {
   FormItem
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Plus, Smile } from "lucide-react";
+import EmojiPicker from "@/components/emoji-picker";
+import { Plus } from "lucide-react";
 import qs from "query-string";
 import { useModal } from "@/hooks/use-modal-store";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -30,6 +32,7 @@ const formSchema = z.object({
 
 function ChatInput({ apiUrl, query, name, type }: ChatInputProps) {
   const { onOpen } = useModal();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -49,6 +52,8 @@ function ChatInput({ apiUrl, query, name, type }: ChatInputProps) {
       });
 
       await fetch(url, { method: "POST", body: JSON.stringify(values) });
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -79,7 +84,9 @@ function ChatInput({ apiUrl, query, name, type }: ChatInputProps) {
                     {...field}
                   />
                   <div className="absolute top-7 right-8">
-                    <Smile />
+                    <EmojiPicker
+                      onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                    />
                   </div>
                 </div>
               </FormControl>
