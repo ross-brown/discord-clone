55002777a93372ad6ebd75ab588635dd46f58ef5
ross-brown <rbrown_98@yahoo.com>
2024-01-09 22:58:01 -0600

    create chat messages component and api query for messages

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import ChatHeader from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
 
 interface ChannelIdPageProps {
   params: {
@@ -43,7 +44,20 @@ async function ChannelIdPage({ params }: ChannelIdPageProps) {
         type="channel"
       />
       <div className="flex-1">
-        Future Messages
+        <ChatMessages
+          member={member}
+          name={channel.name}
+          chatId={channel.id}
+          type="channel"
+          apiUrl="/api/messages"
+          socketUrl="/api/socket/messages"
+          socketQuery={{
+            channelId: channel.id,
+            serverId: channel.serverId
+          }}
+          paramKey="channelId"
+          paramValue={channel.id}
+        />
       </div>
       <ChatInput
         name={channel.name}
