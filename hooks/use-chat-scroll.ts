f422b7275e64dde2441283d7583bafc78857bbd8
ross-brown <rbrown_98@yahoo.com>
2024-01-10 16:52:33 -0600

    implement chat scroll hook

---
+++ hooks/use-chat-scroll.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+type ChatScrollProps = {
+  chatRef: React.RefObject<HTMLDivElement>;
+  bottomRef: React.RefObject<HTMLDivElement>;
+  shouldLoadMore: boolean;
+  loadMore: () => void;
+  count: number;
+};
+
+export const useChatScroll = ({
+  chatRef,
+  bottomRef,
+  shouldLoadMore,
+  loadMore,
+  count
+}: ChatScrollProps) => {
+  const [hasInitialized, setHasInitialized] = useState(false);
+
+  useEffect(() => {
+    const topDiv = chatRef?.current;
+
+    function handleScroll() {
+      const scrollTop = topDiv?.scrollTop;
+      if (scrollTop === 0 && shouldLoadMore) loadMore();
+      console.log("scrollTop, shouldLoadMore", scrollTop, shouldLoadMore);
+    }
+
+    topDiv?.addEventListener("scroll", handleScroll);
+    console.log("Top added event listner");
+
+    return () => {
+      topDiv?.removeEventListener("scroll", handleScroll);
+    };
+  }, [shouldLoadMore, loadMore, chatRef]);
+
+  useEffect(() => {
+    const bottomDiv = bottomRef?.current;
+    const topDiv = chatRef?.current;
+
+    function shouldAutoScroll() {
+      if (!hasInitialized && bottomDiv) {
+        setHasInitialized(true);
+        return true;
+      }
+
+      if (!topDiv) return false;
+
+      const distanceFromBottom = topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
+      return distanceFromBottom <= 100;
+    }
+
+    if (shouldAutoScroll()) {
+      setTimeout(() => {
+        bottomRef.current?.scrollIntoView({
+          behavior: "smooth"
+        });
+      }, 100);
+    }
+  }, [bottomRef, chatRef, count, hasInitialized]);
+
+};
