65c9411dfab060490d09ec5779b663640ee65a92
ross-brown <rbrown_98@yahoo.com>
2024-01-11 16:37:37 -0600

    add send btn for chat in mobile view

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -16,6 +16,7 @@ import { Plus } from "lucide-react";
 import qs from "query-string";
 import { useModal } from "@/hooks/use-modal-store";
 import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -76,17 +77,25 @@ function ChatInput({ apiUrl, query, name, type }: ChatInputProps) {
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
-                  </button>
+                  </button> {/** PLUS BUTTON */}
                   <Input
                     disabled={isLoading}
                     className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                     placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
                     {...field}
                   />
-                  <div className="absolute top-7 right-8">
-                    <EmojiPicker
-                      onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
-                    />
+                  <div className="absolute md:top-7 md:right-8 top-5 right-5">
+                    <div className="flex items-center justify-center gap-x-4">
+                      <EmojiPicker
+                        onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                      />
+                      <Button
+                        type="submit"
+                        variant="primary"
+                        className="md:hidden">
+                        Send
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </FormControl>
